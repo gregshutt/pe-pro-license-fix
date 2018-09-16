7205a0b8cf0788dc247f4589e506b1223e38c259
Greg Shutt <github@gregshutt.com>
2018-09-15 22:07:42 -0400

    adding functional to clear license

--- PEProLicenseFix/Main.cpp
+++ PEProLicenseFix/Main.cpp
@@ -32,6 +32,37 @@ int readSector(const wchar_t* disk, char* buffer, unsigned int sector)
 	return(0);
 }
 
+int writeSector(const wchar_t* disk, char* buffer, unsigned int sector)
+{
+	DWORD dwWritten;
+	HANDLE hDisk = CreateFile(disk, GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, 0);
+
+	if (hDisk == INVALID_HANDLE_VALUE) {
+		CloseHandle(hDisk);
+		fprintf(stderr, "Could not open disk %ls: 0x%x\n", disk, GetLastError());
+		return(-1);
+	}
+
+	if (SetFilePointer(hDisk, sector * 512, NULL, FILE_BEGIN) == INVALID_SET_FILE_POINTER) {
+		CloseHandle(hDisk);
+		return(-1);
+	}
+
+	if (WriteFile(hDisk, buffer, 512, &dwWritten, NULL) == FALSE) {
+		CloseHandle(hDisk);
+		return(-1);
+	}
+
+	if (dwWritten != 512) {
+		CloseHandle(hDisk);
+		return(-1);
+	}
+
+	CloseHandle(hDisk);
+
+	return(0);
+}
+
 int main()
 {
 	char buffer[512];
@@ -46,18 +77,28 @@ int main()
 
 	// read sector 60
 	printf("Checking for license sector...");
-	if (readSector(L"\\\\.\\PhysicalDrive0", buffer, 0) != 0) {
+	if (readSector(L"\\\\.\\PhysicalDrive0", buffer, 60) != 0) {
 		fprintf(stderr, "Could not read first sector");
 		exit(1);
 	}
 
-	if (buffer[508] == 0x00 && buffer[509] == 0x45 && buffer[510] == 0x56 && buffer[511] == 0x1D) {
+	UINT32 magic = buffer[511] << 24 | buffer[510] << 16 | buffer[509] << 8 | buffer[508];
+
+	if (magic == 0x0045561D) {
 		printf("found.\n");
 	}
 	else {
-		printf("not found. Are you sure PE Pro is installed?\n");
+		printf("not found. Are you sure PE Pro is installed? (Expected 0x0045561D, got 0x%0x)\n", magic);
 		exit(0);
 	}
 
-	getchar();
+	memset(buffer, 0, sizeof(buffer));
+
+	printf("Clearing license...");
+	if (writeSector(L"\\\\.\\PhysicalDrive0", buffer, 60) != 0) {
+		printf("failed.\n");
+	}
+	else {
+		printf("succeeded.\n");
+	}
 }
